a370eba485425b3517b60c6046eedefbeb0f4be5
iotprog <76802192+iotprog@users.noreply.github.com>
2020-12-31 15:35:09 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function reply(reply_token) {
         replyToken: reply_token,
         messages: [{
             type: 'text',
-            text: 'Hello'
+            text: 'สวัสดี'
         },
         {
             type: 'text',
