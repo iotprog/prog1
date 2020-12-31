bd64bbd358fb7939a4f8317fb798a8d22e1421e3
iotprog <76802192+iotprog@users.noreply.github.com>
2020-12-31 15:54:21 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -7,17 +7,36 @@ const app = express()
 const port = process.env.PORT || 4000
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+var msgs;
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
+     msgs = req.body.events[0].message.text // message incoming
     reply(reply_token)
     res.sendStatus(200)
 })
+
 app.listen(port)
 function reply(reply_token) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {jzFv8VNiyxZHOgLSRyTbNyt2/gbcazscYCiKnu+1P8dAkhkTTPw0ztA+GAY2ZX0Gn6jnksu4JrUFiG9/2FMPiE4qavhKjg9NPIU7/xHFwUmTej+HfuyJiw3JsWlFhtcXmPrxWcCHrnJmAWcZ5LKIjAdB04t89/1O/w1cDnyilFU=}'
     }
+    if(msgs == "ที่ไหน"){
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+              
+            
+                "type": "location",
+                "title": "บ้านสวน",
+                "address": "ที่กบดาน อิอิ", 
+                "latitude": 13.860480,
+                "longitude": 100.575768
+            }
+            ]
+        })
+    }
+
     let body = JSON.stringify({
         replyToken: reply_token,
         messages: [{
@@ -37,6 +56,7 @@ function reply(reply_token) {
         }
         ]
     })
+
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
         headers: headers,
@@ -44,4 +64,6 @@ function reply(reply_token) {
     }, (err, res, body) => {
         console.log('status = ' + res.statusCode);
     });
+
+
 }
